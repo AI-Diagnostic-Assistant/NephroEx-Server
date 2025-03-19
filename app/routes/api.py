9c6596deed50984b82f0fe945a5a5aa8b7c90aad
Simen Klemp Wergeland <simenklemp@Simen-sin-MacBook-Pro.local>
2025-03-19 14:55:51 +0100

    analysis per kidney

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,17 +1,17 @@
 import io
 import pydicom
 from flask import request, jsonify, Blueprint
-from app.logic.logic import run_single_classification_cnn, \
-    perform_svm_analysis, group_2_min_frames, save_summed_frames_to_storage, save_total_dicom, create_ROI_contours_png, \
-    save_png, perform_decision_tree_analysis, create_and_overlay_heatmaps, save_composite_heatmaps, \
-    fetch_model_from_supabase, load_training_data_supabase
+from app.logic.logic import perform_datapoints_analysis, perform_features_analysis, group_2_min_frames_and_save_to_storage, \
+    save_total_dicom, create_ROI_contours_png, \
+    save_png, \
+    fetch_model_from_supabase, load_training_data_supabase, align_masks_over_frames, align_masks_over_summed_frames, \
+    predict_kidney_masks, create_composite_image_rgb, create_renogram
 from app.client import authenticate_request, create_service_account_client
 
 api = Blueprint('api', __name__)
 
 supabase_client = create_service_account_client()
 
-cnn_model = fetch_model_from_supabase(supabase_client, "best_3dcnn_model.pth", is_cnn_model=True)
 svm_model = fetch_model_from_supabase(supabase_client, "svm_model_summed.joblib")
 svm_scaler = fetch_model_from_supabase(supabase_client, "svm_scaler_summed.joblib")
 svm_training_data = load_training_data_supabase(supabase_client, "svm_training_data.npy")
@@ -44,24 +44,30 @@ def classify(supabase_client):
     dicom_stream = dicom_file[0].stream.read()
     dicom_read = pydicom.dcmread(io.BytesIO(dicom_stream))
 
+    # Save original and grouped 2 min frames to storage
     dicom_storage_id = save_total_dicom(dicom_stream, supabase_client)
+    grouped_2_min_storage_ids = group_2_min_frames_and_save_to_storage(dicom_read, supabase_client)
 
-    grouped_frames = group_2_min_frames(dicom_read)
+    # Create composite image of the request file
+    composite_image = create_composite_image_rgb(dicom_read)
 
-    storage_ids = save_summed_frames_to_storage(grouped_frames, supabase_client)
+    # Predict kidney masks
+    left_mask, right_mask = predict_kidney_masks(composite_image, unet_model)
 
-    # CNN and SVM predictions
-    cnn_predicted, cnn_confidence = run_single_classification_cnn(dicom_read, cnn_model)
-    svm_predicted, svm_confidence, roi_activity_array, left_mask, right_mask, total_activities, shap_data_svm, svm_textual_explanation = perform_svm_analysis(dicom_read, svm_model, svm_scaler, svm_training_data, unet_model)
-    decision_tree_predicted, decision_tree_confidence, shap_data_dt, decision_tree_textual_explanation = perform_decision_tree_analysis(total_activities, dt_model, dt_training_data)
+    # align masks over all frames in the original dicom file
+    left_mask_alignments, right_mask_alignments = align_masks_over_frames(left_mask, right_mask, dicom_read)
+    left_mask_alignments_summed, right_mask_alignments_summed = align_masks_over_summed_frames(left_mask, right_mask, dicom_read)
 
-    svm_predicted_label = "healthy" if svm_predicted == 0 else "sick"
-    cnn_predicted_label = "healthy" if cnn_predicted == 0 else "sick"
-    decision_tree_predicted_label = "healthy" if decision_tree_predicted == 0 else "sick"
+    # Create renogram over all frames from the predicted masks (USED FOR ONLY DISPLAY FOR CURVES)
+    left_activities, right_activities = create_renogram(left_mask_alignments, right_mask_alignments)
+    roi_activity_array = [left_activities.tolist(), right_activities.tolist()]
 
-    heatmap_overlays = create_and_overlay_heatmaps(dicom_read)
+    # Create renogram over 2 min summed frames from the predicted masks (USED FOR PREDICTION)
+    left_activities_summed, right_activities_summed = create_renogram(left_mask_alignments_summed, right_mask_alignments_summed)
 
-    storage_heatmap_paths = save_composite_heatmaps(heatmap_overlays, supabase_client)
+    # Datapoint and feature importance uto classifications
+    left_uto_classsification_datapoints, right_uto_classification_datapoints, left_uto_confidence_datapoints, right_uto_confidence_datapoints, shap_data_left_uto_classification_datapoints, shap_data_right_uto_classification_datapoints, left_textual_explanation_datapoints, right_textual_explanation_datapoints, classified_left_label_datapoints, classified_right_label_datapoints = perform_datapoints_analysis(svm_model, svm_scaler, svm_training_data, left_activities_summed, right_activities_summed)
+    left_uto_classification_features, right_uto_classification_features, left_uto_confidence_features, right_uto_confidence_features, shap_data_left_uto_classification_features, shap_data_right_uto_classification_features, left_textual_explanation_features, right_textual_explanation_features, classified_left_label_features, classified_right_label_features = perform_features_analysis(left_activities, right_activities, dt_model, dt_training_data)
 
     # Create and upload ROI contours
     transparent_contour_image = create_ROI_contours_png(left_mask, right_mask)
@@ -73,7 +79,7 @@ def classify(supabase_client):
             supabase_client.table("report")
             .insert({
                 "patient_id": patient_id,
-                "dicom_storage_ids": storage_ids,
+                "dicom_storage_ids": grouped_2_min_storage_ids,
                 "patient_dicom_storage_id": dicom_storage_id,
                 "roi_contour_object_path": roi_contour_object_path,
             })
@@ -93,11 +99,6 @@ def classify(supabase_client):
                     "category": "renogram",
                     "roi_activity": roi_activity_array,
                 },
-                {
-                    "report_id": report_id,
-                    "category": "image",
-                },
-
             ])
             .execute()
         )
@@ -106,28 +107,37 @@ def classify(supabase_client):
             return jsonify({'error': 'Failed to insert analyses'}), 500
 
         renogram_analysis_id = analysis_response.data[0]["id"]
-        image_analysis_id = analysis_response.data[1]["id"]
 
         classification_response = (
             supabase_client.table("classification")
             .insert([
                 {
                     "analysis_id": renogram_analysis_id,
-                    "prediction": svm_predicted_label,
-                    "confidence": float(svm_confidence),
+                    "prediction": classified_left_label_datapoints,
+                    "confidence": float(left_uto_confidence_datapoints),
                     "type": "svm",
-                },
-                {
-                    "analysis_id": image_analysis_id,
-                    "prediction": cnn_predicted_label,
-                    "confidence": float(cnn_confidence),
-                    "type": "cnn",
+                    "kidney_label": "left",
                 },
                 {
                     "analysis_id": renogram_analysis_id,
-                    "prediction": decision_tree_predicted_label,
-                    "confidence": float(decision_tree_confidence),
+                    "prediction": classified_right_label_datapoints,
+                    "confidence": float(right_uto_confidence_datapoints),
+                    "type": "svm",
+                    "kidney_label": "right",
+                },
+                {
+                    "analysis_id": renogram_analysis_id,
+                    "prediction": classified_left_label_features,
+                    "confidence": float(left_uto_confidence_features),
                     "type": "decision_tree",
+                    "kidney_label": "left",
+                },
+                {
+                    "analysis_id": renogram_analysis_id,
+                    "prediction": classified_right_label_features,
+                    "confidence": float(right_uto_confidence_features),
+                    "type": "decision_tree",
+                    "kidney_label": "right",
                 },
             ])
             .execute()
@@ -136,30 +146,38 @@ def classify(supabase_client):
         if not classification_response.data or len(classification_response.data) < 2:
             return jsonify({'error': 'Failed to insert classifications'}), 500
 
-        svm_classification_id = classification_response.data[0]["id"]
-        cnn_classification_id = classification_response.data[1]["id"]
-        decision_tree_classification_id = classification_response.data[2]["id"]
+        datapoints_classification_left_id = classification_response.data[0]["id"]
+        datapoints_classification_right_id = classification_response.data[1]["id"]
+        features_classification_left_id = classification_response.data[2]["id"]
+        features_classification_right_id = classification_response.data[3]["id"]
 
         explanation_response = (
             supabase_client.table("explanation")
             .insert([
                 {
-                    "classification_id": cnn_classification_id,
-                    "technique": "Grad-CAM",
-                    "heatmap_object_paths": storage_heatmap_paths
+                    "classification_id": datapoints_classification_left_id,
+                    "technique": "SHAP",
+                    "description": left_textual_explanation_datapoints,
+                    "shap_values_renogram_summed": shap_data_left_uto_classification_datapoints
                 },
                 {
-                    "classification_id": svm_classification_id,
+                    "classification_id": datapoints_classification_right_id,
                     "technique": "SHAP",
-                    "description": svm_textual_explanation,
-                    "shap_values_renogram_summed": shap_data_svm
+                    "description": right_textual_explanation_datapoints,
+                    "shap_values_renogram_summed": shap_data_right_uto_classification_datapoints
                 },
                 {
-                    "classification_id": decision_tree_classification_id,
+                    "classification_id": features_classification_left_id,
                     "technique": "SHAP",
-                    "description": decision_tree_textual_explanation,
-                    "shap_values_renogram": shap_data_dt
-                }
+                    "description": left_textual_explanation_features,
+                    "shap_values_renogram": shap_data_left_uto_classification_features
+                },
+                {
+                    "classification_id": features_classification_right_id,
+                    "technique": "SHAP",
+                    "description": right_textual_explanation_features,
+                    "shap_values_renogram": shap_data_right_uto_classification_features
+                },
             ])
             .execute()
         )
